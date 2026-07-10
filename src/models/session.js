16667f466873749cb0073bf7aaef2544c4b8c533
VitaliiDevX <vitalii.dev.x@gmail.com>
2026-07-10 13:12:16 +0300

    feat: implement session authentication and security middleware

---
+++ src/models/session.js
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+const sessionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  { timestamps: true },
+);
+
+sessionSchema.index({ accessToken: 1 });
+sessionSchema.index({ refreshToken: 1 });
+
+export const Session = model('Session', sessionSchema);
